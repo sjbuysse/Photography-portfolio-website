b989cb86f3d64be524929813b97160d6a24bb4a2
Simon Buysse <buysse.simon@gmail.com>
2015-10-15 23:16:02 +0800

    added devise for users

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'carrierwave', '0.10.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+gem 'devise', '3.5.1'
 # Use Unicorn as the app server
 # gem 'unicorn'
 
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,6 +1,8 @@
 class Gallery < ActiveRecord::Base
     has_many :pictures, dependent: :destroy
 
+    mount_uploader :thumb, ThumbUploader
+
     validates :title, presence: true, length: {minimum: 3}
     validates :synopsis, presence: true, length: {minimum: 3}
 end
